b045f4621b2e0f09422804bdb0a0bdf17fca2c9a
Geert Van Laethem <geert.van.laethem@telenet.be>
2022-06-13 15:11:39 +0200

    Get the data out of the hahs

--- 2015/day4/src/main.rs
+++ 2015/day4/src/main.rs
@@ -1,3 +1,14 @@
+use md5::{Md5, Digest};
+
 fn main() {
-    println!("Hello, world!");
+    let mut hasher = Md5::new();
+    hasher.update("abcdef609043");
+    let result = hasher.finalize();
+
+    for r in result {
+        let f = format!("{:x}", r);
+        println!("{:0>2}", f);
+    }
+    
+    //println!("result: {:?} {:x}", result, result);
 }
