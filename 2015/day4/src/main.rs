571b68b73a6afb635f0a6ee1661f5de026e6f33e
Geert Van Laethem <geert@wearesage.be>
2022-06-13 16:26:30 +0200

    Finished day4

--- 2015/day4/src/main.rs
+++ 2015/day4/src/main.rs
@@ -1,14 +1,27 @@
 use md5::{Md5, Digest};
 
 fn main() {
-    let mut hasher = Md5::new();
-    hasher.update("abcdef609043");
-    let result = hasher.finalize();
+    let mut counter = 0;
+    let input = "ckczppom";
+    let mut found = false;
+    while !found {
+        counter += 1;
+        let mut hasher = Md5::new();
 
-    for r in result {
-        let f = format!("{:x}", r);
-        println!("{:0>2}", f);
+        hasher.update(format!("{}{}", input, counter));
+
+        let result = hasher.finalize();
+        let mut str = String::new();
+        for r in result {
+            let f = format!("{:0>2x}", r);
+            str.push_str(&*f);
+        }
+
+        let first_five_chars = &str[..6];
+        if first_five_chars == "000000" {
+            found = true;
+        }
     }
-    
-    //println!("result: {:?} {:x}", result, result);
+
+    println!("counter {}", counter);
 }
